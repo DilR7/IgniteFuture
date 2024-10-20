c7ecf7329e0f8726448321bf2ab929eb25b4aafd
Marco Dave <marcodave03@gmail.com>
2024-10-20 17:43:14 +0700

    migrate update

---
+++ database/migrations/2024_10_20_100004_create_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id('bookID');
+            $table->string('book_name');
+            $table->text('book_desc');
+            $table->string('book_content');
+            $table->timestamps();
+            $table->foreignId('moduleID')->constrained('modules','moduleID')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
