62905435b7e0da4216e9347297e6f329a71d6225
Marco Dave <marcodave03@gmail.com>
2024-10-22 03:13:41 +0700

    add main page

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('main');
+    return view('testing.Halaman1');
 });
 
 
