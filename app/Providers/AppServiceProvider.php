7e00efc2cd4ecad152d8d08096b1aa10efa3fd3d
DilR7 <erictianto12@gmail.com>
2024-12-04 10:10:04 +0700

    Modify App AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
-        else if (env('APP_ENV') === 'local') {
-            URL::forceScheme('http');
-        }
+        // else if (env('APP_ENV') === 'local') {
+        //     URL::forceScheme('http');
+        // }
     }
 }
